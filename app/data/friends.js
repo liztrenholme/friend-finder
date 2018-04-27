29d853c6bc0063470808fea20251926af205e0c8
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-04-27 19:16:31 -0400

    front end all purty with css now

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 // logic for matching up new friends and storing data
 
-var ahmed = {
+var friends = [{
   "name" : "Ahmed",
   "photo" : "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores" : [
@@ -15,4 +15,23 @@ var ahmed = {
       4,
       1
     ]
-};
+},
+
+{
+  "name" : "Dudley",
+  "photo" : "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/1891172_10202092659412030_1613322385_n.jpg?_nc_cat=0&oh=68ba83f84c98a265c78910d26946f5de&oe=5B4F0461",
+  "scores" : [
+      5,
+      4,
+      3,
+      4,
+      1,
+      3,
+      4,
+      2,
+      3,
+      4,
+    ]
+}];
+
+module.exports = friends;
