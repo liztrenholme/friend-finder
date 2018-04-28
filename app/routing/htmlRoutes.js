fc8cae53e0df684d6a1d205f60237abac5e14bcf
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-04-27 22:00:36 -0400

    changed home.html to index.html, hoping css will work on heroku

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,12 +4,14 @@ var express = require("express");
 module.exports = function () {
   var router = express.Router();
 
+//displays survey page if "/survey" is entered into address bar
   router.get('/survey', function (req, res) {
     return res.sendFile(path.join(__dirname, '../public/survey.html'));
   });
 
+//displays homepage if anything other than "/survey" is entered into address bar
   router.get('*', function (req, res) {
-    return res.sendFile(path.join(__dirname, "../public/home.html"));
+    return res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   return router;
