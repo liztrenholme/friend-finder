fbd9b138f091be7861b05c5ceb42a8e15d658b38
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-04-28 09:04:17 -0400

    modal working, but friendmatch logic needs debugged

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,51 @@
-
-
 var friends = [{
-  "name" : "Ahmed",
-  "photo" : "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores" : [
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
-    ]
-},
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
 
-{
-  "name" : "Dudley",
-  "photo" : "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/1891172_10202092659412030_1613322385_n.jpg?_nc_cat=0&oh=68ba83f84c98a265c78910d26946f5de&oe=5B4F0461",
-  "scores" : [
-      5,
-      4,
-      3,
-      4,
-      1,
-      3,
-      4,
-      2,
-      3,
-      4
-    ]
-},
-{
-  "name" : "Paul",
-  "photo" : "#",
-  "scores" : [
-      4,
-      5,
-      2,
-      2,
-      2,
-      3,
-      5,
-      5,
-      1
-    ]
-}];
+    {
+        "name": "Dudley",
+        "photo": "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/1891172_10202092659412030_1613322385_n.jpg?_nc_cat=0&oh=68ba83f84c98a265c78910d26946f5de&oe=5B4F0461",
+        "scores": [
+            5,
+            4,
+            3,
+            4,
+            1,
+            3,
+            4,
+            2,
+            3,
+            4
+        ]
+    },
+    {
+        "name": "Paul",
+        "photo": "#",
+        "scores": [
+            4,
+            5,
+            2,
+            2,
+            2,
+            3,
+            5,
+            5,
+            1
+        ]
+    }
+];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,16 +6,33 @@ module.exports = function () {
   var router = express.Router();
 
   router.get('/api/friends', function (req, res) {
-    return res.json(friends);
+    res.json(friends);
   });
 
   router.post('/api/friends', function (req, res) {
-
    friends.push(req.body);
-   res.json(req.body);
-    return res.json(true);
-    console.log(req.body);
+   console.log(friends);
+     var match = false;
+     var friendMatch;
+     var totalDifference = 0;
+
+    //loops through friends 
+    for (var i = 0; i < friends.length; i++) {
+      for (var j = 0; j < req.body.scores.length; j++) {
+        totalDifference = Math.abs(req.body.scores[j] - friends[i].scores[j]);
+
+      }
+  if (totalDifference > 30) {
+    match = true;
+  }
+      console.log(match);
+     friendMatch = friends[i];
+     console.log(friendMatch);
+    }
+    res.json(friendMatch);
+     friends.push(req.body);
   });
+
 
   return router;
 };
