882b36b503fc3d0d2096b0b6bd554c1bc1c23b3f
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-04-28 09:13:49 -0400

    added comments, modal still showing user as friendmatch though

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,7 @@ var friends = require("../data/friends");
 
 module.exports = function () {
   var router = express.Router();
-
+// uses express router to grab data from friends.js file
   router.get('/api/friends', function (req, res) {
     res.json(friends);
   });
@@ -20,15 +20,15 @@ module.exports = function () {
     for (var i = 0; i < friends.length; i++) {
       for (var j = 0; j < req.body.scores.length; j++) {
         totalDifference = Math.abs(req.body.scores[j] - friends[i].scores[j]);
-
+        // compares scores and calculates total difference between friends
       }
-  if (totalDifference > 30) {
+  if (totalDifference > 30) { // threshold for friendship
     match = true;
   }
       console.log(match);
      friendMatch = friends[i];
      console.log(friendMatch);
-    }
+    } 
     res.json(friendMatch);
      friends.push(req.body);
   });
